65d2e6a5eedf46b1c37f00f6c0fe690d4378f71c
DRUPAL23 <103066830+DRUPAL23@users.noreply.github.com>
2026-08-26 22:49:22 +0300

    feat(api): register demo controller

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -3,6 +3,7 @@ import { PrismaService } from './prisma.service';
 import { MatchController } from './match.controller';
 import { MatchService } from './match.service';
 import { MatchGateway } from './match.gateway';
+import { DemoController } from './demo.controller';
 
-@Module({ controllers: [MatchController], providers: [PrismaService, MatchService, MatchGateway] })
+@Module({ controllers: [MatchController, DemoController], providers: [PrismaService, MatchService, MatchGateway] })
 export class AppModule {}
